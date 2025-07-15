9df59836363af35aea31685a03b6612efcd3f9b2
yinchao <ycsuper2819@gmail.com>
2025-07-15 22:49:56 +0800

    style：更新文件头部信息，修正作者和最后编辑时间

--- sina/main.py
+++ sina/main.py
@@ -1,8 +1,8 @@
 '''
 Author: error: git config user.name & please set dead value or install git
 Date: 2025-07-13 23:23:03
-LastEditors: error: git config user.name & please set dead value or install git
-LastEditTime: 2025-07-13 23:23:14
+LastEditors: yinchao
+LastEditTime: 2025-07-14 17:01:22
 Description: 
 '''
 from scrapy import cmdline
