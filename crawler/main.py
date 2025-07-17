3c20bb57db25ea69e6029541317668f2ae6fbf5e
yinchao <ycsuper2819@gmail.com>
2025-07-17 09:30:47 +0800

    feat：调整工作目录以确保爬虫正确执行

--- crawler/main.py
+++ crawler/main.py
@@ -1,8 +1,14 @@
 '''
+author: yinchao
+date: Do not edit
+team: wuhan operational dev.
+Description: 
+'''
+'''
 Author: error: git config user.name & please set dead value or install git
 Date: 2025-07-13 23:23:03
-LastEditors: yinchao
-LastEditTime: 2025-07-15 23:20:05
+LastEditors: Please set LastEditors
+LastEditTime: 2025-07-17 09:27:15
 Description: 
 '''
 import sys
@@ -13,5 +19,10 @@ from scrapy import cmdline
 # sina/main.py -> 向上一级到项目根目录
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+
 if __name__ == '__main__':
+    # 切换到scrapy项目目录运行
+    # current_dir = os.getcwd()
+    scrapy_project_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sina')
+    os.chdir(scrapy_project_dir)
     cmdline.execute('scrapy crawl sina_spider -s LOG_FILE=spider.log'.split())
